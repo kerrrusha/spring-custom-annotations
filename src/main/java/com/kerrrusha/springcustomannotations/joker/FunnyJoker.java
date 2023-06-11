28f872ca1a3f160ae8a123b67be452e24f765b85
Kirill Koval <brcyrill2@gmail.com>
2023-06-12 02:02:13 +0300

    added 3-rd phase constructors

--- src/main/java/com/kerrrusha/springcustomannotations/joker/FunnyJoker.java
+++ src/main/java/com/kerrrusha/springcustomannotations/joker/FunnyJoker.java
@@ -2,7 +2,12 @@ package com.kerrrusha.springcustomannotations.joker;
 
 import com.kerrrusha.springcustomannotations.annotation.InjectRandomInt;
 import com.kerrrusha.springcustomannotations.annotation.InjectRandomJoke;
+import com.kerrrusha.springcustomannotations.annotation.PostProxiesCreated;
+import jakarta.annotation.PostConstruct;
 import lombok.Setter;
+
+import static com.kerrrusha.springcustomannotations.SpringCustomAnnotationsApplication.logApplicationContextCreated;
+import static java.util.Objects.nonNull;
 
 @Setter
 public class FunnyJoker implements Joker {
@@ -12,6 +17,37 @@ public class FunnyJoker implements Joker {
 
     @InjectRandomJoke
     private String joke;
+
+    public FunnyJoker() {
+        System.out.println("Constructor Phase I");
+        logInnerFieldsAreInitialized();
+        logApplicationContextCreated();
+        System.out.println();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Constructor Phase II");
+        logInnerFieldsAreInitialized();
+        logApplicationContextCreated();
+        System.out.println();
+    }
+
+    @PostProxiesCreated
+    public void lateInit() {
+        System.out.println("Constructor Phase III");
+        logInnerFieldsAreInitialized();
+        logApplicationContextCreated();
+        System.out.println();
+    }
+
+    private void logInnerFieldsAreInitialized() {
+        System.out.println("Inner fields are initialized: " + innerFieldsAreInitialized());
+    }
+
+    private boolean innerFieldsAreInitialized() {
+        return nonNull(joke);
+    }
 
     @Override
     public void sayJoke() {
