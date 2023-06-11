c59651ece4a935a83a51b0c9635b37c3fe52e054
Kirill Koval <brcyrill2@gmail.com>
2023-06-12 02:02:29 +0300

    added logging of context being created

--- src/main/java/com/kerrrusha/springcustomannotations/SpringCustomAnnotationsApplication.java
+++ src/main/java/com/kerrrusha/springcustomannotations/SpringCustomAnnotationsApplication.java
@@ -1,13 +1,27 @@
 package com.kerrrusha.springcustomannotations;
 
 import com.kerrrusha.springcustomannotations.joker.Joker;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static java.util.Objects.nonNull;
 
 public class SpringCustomAnnotationsApplication {
 
+	private static ApplicationContext context;
+
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
-		ctx.getBean(Joker.class).sayJoke();
+		context = new ClassPathXmlApplicationContext("context.xml");
+
+		System.out.print(SpringCustomAnnotationsApplication.class.getName() + " - ");
+		logApplicationContextCreated();
+		System.out.println();
+
+		context.getBean(Joker.class).sayJoke();
+	}
+
+	public static void logApplicationContextCreated() {
+		System.out.println("Application context created: " + nonNull(context));
 	}
 
 }
